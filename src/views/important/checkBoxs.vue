bdd47df494650966ff4c057586a80198b3cf80cf
shining0515 <lgl_137@163.com>
2019-03-28 17:33:53 +0800

    重点业务监控

--- src/views/important/checkBoxs.vue
+++ src/views/important/checkBoxs.vue
@@ -1,252 +1,304 @@
 <template>
-    <div>
-        <div class="flexcc">
-          <div style="color:#F84252">注：（指标最多选择三条）</div>
-          <el-radio-group v-model="dateType" style="margin:0 20px;">
-            <el-radio label="D">日</el-radio>
-            <el-radio label="M">月</el-radio>
-          </el-radio-group>
-          <el-date-picker v-show="dateType=='D'" v-model="dateD" type="date" placeholder="选择日期"></el-date-picker>
-          <el-date-picker v-show="dateType=='M'" v-model="dateM" type="month" placeholder="选择日期"></el-date-picker>
-        </div>
-        <ul class="checkBoxUL">
-          <li v-for="(typeLi,liIndex) in checkOptions" :key="liIndex">
-            <div class="li-title" :id="typeLi.PRO_TYPE">{{typeLi.PRO_TYPE_NAME}}</div>
-              <el-checkbox-group 
-                v-model="typeLi.checkedIds"
-                :min="0"
-                :max="3">
-              <el-checkbox v-for="(product,index) in typeLi.productList" 
-                :label="product.PRODUCT_CODE" 
-                :checked="product.IS_DEFAULT==1"
-                @change="changeBox()"
-                :key="index">
-                {{product.PRODUCT_NAME}}
-              </el-checkbox>
-            </el-checkbox-group>
-          </li>
-        </ul>
-        <div class="footBtns">
-          <div class="btn" @click="sendMsg(false)">取消</div>
-          <div class="btn okBtn" @click="sendMsg(true)">确定</div>
-        </div>
+  <div>
+    <div class="flexcc">
+      <div style="color:#F84252">注：（指标最多选择三条）</div>
+      <el-radio-group v-model="dateType" style="margin:0 20px;">
+        <el-radio label="D">日</el-radio>
+        <el-radio label="M">月</el-radio>
+      </el-radio-group>
+      <el-date-picker
+        format="yyyy-MM-dd"
+        value-format="yyyyMMdd"
+        v-show="dateType=='D'"
+        v-model="dateD"
+        type="date"
+        placeholder="选择日期"
+      ></el-date-picker>
+      <el-date-picker
+        format="yyyy-MM"
+        value-format="yyyyMM"
+        v-show="dateType=='M'"
+        v-model="dateM"
+        type="month"
+        placeholder="选择日期"
+      ></el-date-picker>
     </div>
+    <ul class="checkBoxUL">
+      <li v-for="(typeLi,liIndex) in checkOptions" :key="liIndex">
+        <div class="li-title" :id="typeLi.PRO_TYPE">{{typeLi.PRO_TYPE_NAME}}</div>
+        <el-checkbox-group v-model="typeLi.checkedIds" :min="0" :max="3">
+          <el-checkbox
+            v-for="(product,index) in typeLi.productList"
+            :label="product.PRODUCT_CODE"
+            :checked="product.IS_DEFAULT==1"
+            @change="changeBox()"
+            :key="index"
+          >{{product.PRODUCT_NAME}}</el-checkbox>
+        </el-checkbox-group>
+      </li>
+    </ul>
+    <div class="footBtns">
+      <div class="btn" @click="sendMsg(false)">取消</div>
+      <div class="btn okBtn" @click="sendMsg(true)">确定</div>
+    </div>
+  </div>
 </template>
 
 <script>
+import qs from "qs";
 export default {
   data() {
     return {
-      dateD:"",
-      dateM:"",
-      dateType:"",
+      dateD: "",
+      dateM: "",
+      dateType: "",
       checkedIds: [],
       checkOptions: []
     };
   },
-  methods:{
+  methods: {
+    post() {
+      let _this = this;
+      this.$axios
+        .get("/Workbench/getKeyTarget")
+        .then(function(res) {
+          let resultData = res.data.resultData;
+          _this.dateType = resultData.MORD;
+          _this.dateD = resultData.dayId;
+          _this.dateM = resultData.monthId;
+          _this.checkOptions = resultData.rTarget;
+          resultData.rTarget.map((obj, index) => {
+            _this.$set(_this.checkOptions[index], "checkedIds", []);
+          });
+        })
+        .catch(function(e) {
+          console.log(e);
+        });
+    },
     sendMsg(isOk) {
-      if(isOk){
+      if (isOk) {
+        let date = this.dateType === "M" ? "monthId" : "dayId";
         const param = {
-          checkedIds: this.checkedIds
-        }
-        this.$emit('headCallBack', false, param); 
-      }else{
-        this.$emit('headCallBack', false); 
+          MORD: this.dateType,
+          [date]: this.dateType === "D" ? this.dateD : this.dateM,
+          proCodes: this.checkedIds
+        };
+        this.submit(param);
+        this.$emit("headCallBack", false, param);
+      } else {
+        this.$emit("headCallBack", false);
       }
     },
-    setDefault(){
+    setDefault() {
       const resultData = {
-        "MORD":"D",
-        "dayId":"20190302",
-        "monthId":"201902",
-        "rTarget":[
+        MORD: "D",
+        dayId: "20190302",
+        monthId: "201902",
+        rTarget: [
           {
-            "PRO_TYPE":"001",
-            "PRO_TYPE_NAME":"业务类",
-            "productList":[
+            PRO_TYPE: "001",
+            PRO_TYPE_NAME: "业务类",
+            productList: [
               {
-                "XZ_GROUP":"10",
-                "PRODUCT_NAME":"单卡：冰红99/129+尊享红199/398/599/799",
-                "IS_DEFAULT":1,
-                "PRODUCT_CODE":"001001"
+                XZ_GROUP: "10",
+                PRODUCT_NAME: "单卡：冰红99/129+尊享红199/398/599/799",
+                IS_DEFAULT: 1,
+                PRODUCT_CODE: "001001"
               },
               {
-                "XZ_GROUP":"10",
-                "PRODUCT_NAME":"融合：全家红138+尊享红融合199/398/599/799",
-                "IS_DEFAULT":1,
-                "PRODUCT_CODE":"001002"
+                XZ_GROUP: "10",
+                PRODUCT_NAME: "融合：全家红138+尊享红融合199/398/599/799",
+                IS_DEFAULT: 1,
+                PRODUCT_CODE: "001002"
               },
               {
-                "XZ_GROUP":"10",
-                "PRODUCT_NAME":"尊红迁转",
-                "IS_DEFAULT":0,
-                "PRODUCT_CODE":"001003"
+                XZ_GROUP: "10",
+                PRODUCT_NAME: "尊红迁转",
+                IS_DEFAULT: 0,
+                PRODUCT_CODE: "001003"
               },
               {
-                  "XZ_GROUP":"10",
-                  "PRODUCT_NAME":"M计划",
-                  "IS_DEFAULT":1,
-                  "PRODUCT_CODE":"001004"
+                XZ_GROUP: "10",
+                PRODUCT_NAME: "M计划",
+                IS_DEFAULT: 1,
+                PRODUCT_CODE: "001004"
               },
               {
-                  "XZ_GROUP":"10",
-                  "PRODUCT_NAME":"TV增值线下销售情况",
-                  "IS_DEFAULT":0,
-                  "PRODUCT_CODE":"001005"
+                XZ_GROUP: "10",
+                PRODUCT_NAME: "TV增值线下销售情况",
+                IS_DEFAULT: 0,
+                PRODUCT_CODE: "001005"
               },
               {
-                  "XZ_GROUP":"10",
-                  "PRODUCT_NAME":"智家发展",
-                  "IS_DEFAULT":0,
-                  "PRODUCT_CODE":"001006"
+                XZ_GROUP: "10",
+                PRODUCT_NAME: "智家发展",
+                IS_DEFAULT: 0,
+                PRODUCT_CODE: "001006"
               },
               {
-                  "XZ_GROUP":"10",
-                  "PRODUCT_NAME":"自建厅移动发展",
-                  "IS_DEFAULT":0,
-                  "PRODUCT_CODE":"001007"
+                XZ_GROUP: "10",
+                PRODUCT_NAME: "自建厅移动发展",
+                IS_DEFAULT: 0,
+                PRODUCT_CODE: "001007"
               },
               {
-                  "XZ_GROUP":"10",
-                  "PRODUCT_NAME":"核心渠道99及以上",
-                  "IS_DEFAULT":0,
-                  "PRODUCT_CODE":"001008"
+                XZ_GROUP: "10",
+                PRODUCT_NAME: "核心渠道99及以上",
+                IS_DEFAULT: 0,
+                PRODUCT_CODE: "001008"
               },
               {
-                  "XZ_GROUP":"10",
-                  "PRODUCT_NAME":"核心渠道138及以上",
-                  "IS_DEFAULT":0,
-                  "PRODUCT_CODE":"001009"
+                XZ_GROUP: "10",
+                PRODUCT_NAME: "核心渠道138及以上",
+                IS_DEFAULT: 0,
+                PRODUCT_CODE: "001009"
               },
               {
-                  "XZ_GROUP":"10",
-                  "PRODUCT_NAME":"存拉移",
-                  "IS_DEFAULT":0,
-                  "PRODUCT_CODE":"001010"
+                XZ_GROUP: "10",
+                PRODUCT_NAME: "存拉移",
+                IS_DEFAULT: 0,
+                PRODUCT_CODE: "001010"
               },
               {
-                  "XZ_GROUP":"10",
-                  "PRODUCT_NAME":"存拉宽",
-                  "IS_DEFAULT":0,
-                  "PRODUCT_CODE":"001011"
+                XZ_GROUP: "10",
+                PRODUCT_NAME: "存拉宽",
+                IS_DEFAULT: 0,
+                PRODUCT_CODE: "001011"
               },
               {
-                  "XZ_GROUP":"10",
-                  "PRODUCT_NAME":"业务发展有效能人数量",
-                  "IS_DEFAULT":0,
-                  "PRODUCT_CODE":"001012"
+                XZ_GROUP: "10",
+                PRODUCT_NAME: "业务发展有效能人数量",
+                IS_DEFAULT: 0,
+                PRODUCT_CODE: "001012"
               },
               {
-                  "XZ_GROUP":"10",
-                  "PRODUCT_NAME":"宽带融合发展量",
-                  "IS_DEFAULT":0,
-                  "PRODUCT_CODE":"001013"
+                XZ_GROUP: "10",
+                PRODUCT_NAME: "宽带融合发展量",
+                IS_DEFAULT: 0,
+                PRODUCT_CODE: "001013"
               },
               {
-                  "XZ_GROUP":"10",
-                  "PRODUCT_NAME":"社会渠道宽带融合轻工具发展",
-                  "IS_DEFAULT":0,
-                  "PRODUCT_CODE":"001014"
+                XZ_GROUP: "10",
+                PRODUCT_NAME: "社会渠道宽带融合轻工具发展",
+                IS_DEFAULT: 0,
+                PRODUCT_CODE: "001014"
               }
             ]
           },
           {
-            "PRO_TYPE":"002",
-            "PRO_TYPE_NAME":"拆机类",
-            "productList":[
-                {
-                    "XZ_GROUP":"20",
-                    "PRODUCT_NAME":"移动主拆",
-                    "IS_DEFAULT":0,
-                    "PRODUCT_CODE":"002001"
-                },
-                {
-                    "XZ_GROUP":"20",
-                    "PRODUCT_NAME":"移动欠拆",
-                    "IS_DEFAULT":0,
-                    "PRODUCT_CODE":"002002"
-                },
-                {
-                    "XZ_GROUP":"20",
-                    "PRODUCT_NAME":"宽带主拆",
-                    "IS_DEFAULT":0,
-                    "PRODUCT_CODE":"002003"
-                },
-                {
-                    "XZ_GROUP":"20",
-                    "PRODUCT_NAME":"宽带欠拆",
-                    "IS_DEFAULT":0,
-                    "PRODUCT_CODE":"002004"
-                }
+            PRO_TYPE: "002",
+            PRO_TYPE_NAME: "拆机类",
+            productList: [
+              {
+                XZ_GROUP: "20",
+                PRODUCT_NAME: "移动主拆",
+                IS_DEFAULT: 0,
+                PRODUCT_CODE: "002001"
+              },
+              {
+                XZ_GROUP: "20",
+                PRODUCT_NAME: "移动欠拆",
+                IS_DEFAULT: 0,
+                PRODUCT_CODE: "002002"
+              },
+              {
+                XZ_GROUP: "20",
+                PRODUCT_NAME: "宽带主拆",
+                IS_DEFAULT: 0,
+                PRODUCT_CODE: "002003"
+              },
+              {
+                XZ_GROUP: "20",
+                PRODUCT_NAME: "宽带欠拆",
+                IS_DEFAULT: 0,
+                PRODUCT_CODE: "002004"
+              }
             ]
           }
         ]
       };
       this.dateType = resultData.MORD;
-      this.dateD = resultData.dayId.slice(0,4)+"-"+resultData.dayId.slice(4,6)+"-"+resultData.dayId.slice(6,8);
-      this.dateM = resultData.monthId.slice(0,4)+"-"+resultData.monthId.slice(4,6);
+      this.dateD =
+        resultData.dayId.slice(0, 4) +
+        "-" +
+        resultData.dayId.slice(4, 6) +
+        "-" +
+        resultData.dayId.slice(6, 8);
+      this.dateM =
+        resultData.monthId.slice(0, 4) + "-" + resultData.monthId.slice(4, 6);
       this.checkOptions = resultData.rTarget;
-      resultData.rTarget.map((obj,index)=>{
-        this.$set(this.checkOptions[index],'checkedIds',[]);
+      resultData.rTarget.map((obj, index) => {
+        this.$set(this.checkOptions[index], "checkedIds", []);
       });
     },
-    changeBox(){
+    changeBox() {
       this.checkedIds = [];
-      this.checkOptions.map((obj,index)=>{
+      this.checkOptions.map((obj, index) => {
         this.checkedIds = this.checkedIds.concat(obj.checkedIds);
       });
+    },
+    submit(params) {
+      this.$axios
+        .get(
+          "/Workbench/setKeyTarget?" +
+            qs.stringify({ JsonParam: JSON.stringify(params) })
+        )
+        .then(function(res) {
+          console.log("成功了吗？",res)
+        })
+        .catch(e => {});
     }
   },
-  created(){
-    setTimeout(()=>{
-      this.setDefault();
-    },0);
+  created() {
+    // setTimeout(() => {
+    //   this.setDefault();
+    // }, 0);
+    this.post();
   }
-}
+};
 </script>
 
 <style>
-.checkBoxUL .el-checkbox__inner{
+.checkBoxUL .el-checkbox__inner {
   background-color: transparent;
 }
-.el-checkbox__label{
+.el-checkbox__label {
   color: #fff;
 }
 </style>
 <style scoped lang="less">
-.flexcc{
+.flexcc {
   display: flex;
   justify-content: center;
   align-items: center;
 }
-.checkBoxUL{
+.checkBoxUL {
   color: #fff;
-  .li-title{
+  .li-title {
     width: 100px;
     height: 30px;
     line-height: 30px;
     text-align: center;
-    border: 1px solid #5FB6F9;
+    border: 1px solid #5fb6f9;
     border-radius: 3px;
     margin: 10px 0;
   }
-  .el-checkbox-group{
+  .el-checkbox-group {
     display: flex;
     flex-wrap: wrap;
     justify-content: space-between;
-    .el-checkbox{
+    .el-checkbox {
       flex: 0 1 25%;
       margin: 8px 0;
     }
   }
 }
-.footBtns{
+.footBtns {
   display: flex;
   justify-content: center;
   align-items: center;
   margin-top: 20px;
-  .btn{
+  .btn {
     width: 100px;
     border: 1px solid #fff;
     color: #fff;
@@ -258,9 +310,9 @@ export default {
     margin: 0 20px;
     cursor: pointer;
   }
-  .okBtn{
-    border:none;
-    background-image: linear-gradient(-135deg, #9BD9FC 0%, #3096FC 100%);
+  .okBtn {
+    border: none;
+    background-image: linear-gradient(-135deg, #9bd9fc 0%, #3096fc 100%);
   }
 }
 </style>
--- src/views/xiaoqu/index.vue
+++ src/views/xiaoqu/index.vue
@@ -1,52 +1,66 @@
 <template>
   <div class="moveDiv" @click="sendMsg">
-    <card :cardset="xq" :timetype="'month'" style="margin-right:10px;">
+    <card :cardset="xq" :timetype="'month'" :propsTime="cardTime" style="margin-right:10px;">
       <div class="cardCont flexcc">
-        <div style="font-size:56px">456</div>
+        <div style="font-size:56px">{{xqData.XQ_NUM}}</div>
       </div>
     </card>
     <div class="content">
       <card :cardset="sjxq" :timetype="''" style="height:calc(50% - 5px);">
         <div class="cardCont">
           <div class="numDiv flexcc">
-            <el-popover
-              placement="top"
-              width="300"
-              trigger="hover">
+            <el-popover placement="top" width="300" trigger="hover">
               <div style="color:#fff">
-                <p>单元类型（农村/城市）</p>  
-                <p>市公司排名xxx  平均完成率 xxxx</p>
-                <p>同类单元排名 xxxx 平均完成率  xxxxx</p> 
+                <p>单元类型（农村/城市）</p>
+                <p>市公司排名xxx 平均完成率 xxxx</p>
+                <p>同类单元排名 xxxx 平均完成率 xxxxx</p>
                 <p>分公司排名xxx 平均完成率 xxxx</p>
               </div>
               <div slot="reference">456</div>
             </el-popover>
           </div>
           <div class="progressDiv flexcc">
-            <div class="textDiv"><span>占比</span><span>88.88%</span></div>
-            <div class="dark"><el-progress :text-inside="false" :stroke-width="9" :percentage="80.99" :show-text="false"></el-progress></div>
+            <div class="textDiv">
+              <span>占比</span>
+              <span>88.88%</span>
+            </div>
+            <div class="dark">
+              <el-progress
+                :text-inside="false"
+                :stroke-width="9"
+                :percentage="80.99"
+                :show-text="false"
+              ></el-progress>
+            </div>
           </div>
         </div>
       </card>
       <card :cardset="dsxq" :timetype="''" style="height:calc(50% - 5px);">
         <div class="cardCont">
           <div class="numDiv flexcc">
-            <el-popover
-              placement="top"
-              width="300"
-              trigger="hover">
+            <el-popover placement="top" width="300" trigger="hover">
               <div style="color:#fff">
-                <p>单元类型（农村/城市）</p>  
-                <p>市公司排名xxx  平均完成率 xxxx</p>
-                <p>同类单元排名 xxxx 平均完成率  xxxxx</p> 
+                <p>单元类型（农村/城市）</p>
+                <p>市公司排名xxx 平均完成率 xxxx</p>
+                <p>同类单元排名 xxxx 平均完成率 xxxxx</p>
                 <p>分公司排名xxx 平均完成率 xxxx</p>
               </div>
               <div slot="reference">456</div>
             </el-popover>
           </div>
           <div class="progressDiv flexcc">
-            <div class="textDiv"><span>占比</span><span>88.88%</span></div>
-            <div class="dark"><el-progress :text-inside="false" :stroke-width="9" :percentage="80.99" :show-text="false"></el-progress></div>
+            <div class="textDiv">
+              <span>占比</span>
+              <span>88.88%</span>
+            </div>
+            <div class="dark">
+              <el-progress
+                :text-inside="false"
+                :stroke-width="9"
+                :percentage="80.99"
+                :show-text="false"
+              ></el-progress>
+            </div>
           </div>
         </div>
       </card>
@@ -55,6 +69,7 @@
 </template>
 
 <script>
+import qs from "qs";
 import card from "../../components/Card.vue";
 const defaultParam = {
   leftcolor: "#FAD961",
@@ -67,7 +82,7 @@ export default {
       xq: {
         title: "小区数",
         ...defaultParam,
-        width: "calc(50% - 5px)",
+        width: "calc(50% - 5px)"
       },
       sjxq: {
         title: "双降小区",
@@ -76,27 +91,70 @@ export default {
       dsxq: {
         title: "低渗小区",
         ...defaultParam
-      }
+      },
+      xqData: {},
+      date: "",
+      cardTime: ""
     };
   },
   methods: {
-      sendMsg:function() {
+    sendMsg: function() {
       const param = {
-        dialogCompent:"xiaoquSed",
-        dialogTitle:"小区",
+        dialogCompent: "xiaoquSed",
+        dialogTitle: "小区"
+      };
+      this.$emit("headCallBack", param); //第一个参数是父组件中v-on绑定的自定义回调方法，第二个参数为传递的参数
+    },
+    post() {
+      let _this = this;
+      let params = {};
+      this.$axios
+        .get(
+          "/community/getIndexData?" +
+            qs.stringify({ JsonParam: JSON.stringify(params) })
+        )
+        .then(function(res) {
+          let data = res.data;
+          _this.xqData = data.resultData;
+          _this.date = data.resultData.ACCT_MONTH;
+        })
+        .catch(function(e) {
+          console.log(e);
+        });
+    }
+  },
+  created() {
+    this.post();
+  },
+  watch: {
+    date(newValue, oldValue) {
+      this.postLine();
+      if (newValue.search(/-/) === -1 && newValue.search(/年/) === -1) {
+        if (newValue.length === 6) {
+          this.cardTime = [newValue.substr(0, 4), newValue.substr(4, 2)].join(
+            "-"
+          );
+        } else if (newValue.length === 8) {
+          this.cardTime = [
+            newValue.substr(0, 4),
+            newValue.substr(4, 2),
+            newValue.substr(6, 2)
+          ].join("-");
+        }
+      } else {
+        this.cardTime = newValue;
       }
-      this.$emit('headCallBack', param); //第一个参数是父组件中v-on绑定的自定义回调方法，第二个参数为传递的参数
     }
   }
 };
 </script>
 
 <style>
-.progressDiv .dark .el-progress-bar__outer{
-  background: #4C4C4C !important;
+.progressDiv .dark .el-progress-bar__outer {
+  background: #4c4c4c !important;
 }
-.progressDiv .dark .el-progress-bar__inner{
-  background: linear-gradient(-135deg, #FAD961 0%, #F76B1C 100%);
+.progressDiv .dark .el-progress-bar__inner {
+  background: linear-gradient(-135deg, #fad961 0%, #f76b1c 100%);
 }
 </style>
 <style scoped lang="less">
@@ -106,33 +164,33 @@ export default {
   display: flex;
   justify-content: space-between;
 }
-.content{
+.content {
   width: ~"calc(50% - 5px)";
   display: flex;
   flex-direction: column;
   justify-content: space-between;
 }
-.cardCont{
+.cardCont {
   width: 100%;
   height: ~"calc(100% - 30px)";
   display: flex;
 }
-.flexcc{
+.flexcc {
   display: flex;
   justify-content: center;
   align-items: center;
 }
-.numDiv{
+.numDiv {
   flex: 0 0 30%;
   font-size: 30px;
 }
-.progressDiv{
+.progressDiv {
   flex: 0 0 70%;
   flex-direction: column;
-  >div{
+  > div {
     width: 80%;
   }
-  .textDiv{
+  .textDiv {
     display: flex;
     justify-content: space-between;
     margin-bottom: 10px;
